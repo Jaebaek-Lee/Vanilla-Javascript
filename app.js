ec5b41d35ed1c6067ad1944442f1cbbfa8c33797
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-10-29 22:40:45 +0900

    Object

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-// const monday = "mon";
-// const tuesday = "tue";//...
-// const week = monday + tuesday;//...
-// //fuxk...
-// const week2 = [monday, tuesday];
-// //array of js.
-// const nonsense = [1,2,"hello", false, null, true, null, undefined];
-// console.log(week2);
-// console.log(nonsense);
+//배열을 쓰는 경우
+//리스트를 만들고 싶을 때
+const palemember = "이창민, 이재백, 한우석";
+console.log(palemember);
+//활용도 쓰레기
+const palememberArray = ["이창민", "이재백", "한우석"];
+console.log(palememberArray);
+console.log(palememberArray[0]);
+palememberArray.push("하리보");
+console.log(palememberArray);
+//활용도 굳.
+//그러나 한 가지 카테고리에만 통함.
 
-const week = ["mon", "tue", "wed", "thur", "fry", "sat"];
-console.log(week);
-console.log(week[5]);
-console.log(week[6]);
-week.push("sun");
-console.log(week);
-console.log(week[6]);
-
-//JavaScript의 메모리 구조
-//https://curryyou.tistory.com/276
-//https://poiemaweb.com/js-array-is-not-arrray
+/* 객체는 여러가지 속성의 데이터를 가진다. */
+const pale = {
+    name: ["이창민", "이재백", "한우석"],
+    age: 23,
+    male: true,
+    female: false
+};
+console.log(pale);
+console.log(pale.name[0]);
+console.log(pale.male);
+pale.name = "이재백";//const는 pale이 가리킬 주소이지 요소가 아님. 변경 가능
+console.log(pale.name[1]);//재
+pale.location = "discord";
+console.log(pale); //location 추가됨.
