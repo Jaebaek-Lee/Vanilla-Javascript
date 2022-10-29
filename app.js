b7414870970eac76c1e1212f760b75b17f5ca521
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-10-30 00:44:31 +0900

    Object.function

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-//배열을 쓰는 경우
-//리스트를 만들고 싶을 때
-const palemember = "이창민, 이재백, 한우석";
-console.log(palemember);
-//활용도 쓰레기
-const palememberArray = ["이창민", "이재백", "한우석"];
-console.log(palememberArray);
-console.log(palememberArray[0]);
-palememberArray.push("하리보");
-console.log(palememberArray);
-//활용도 굳.
-//그러나 한 가지 카테고리에만 통함.
-
-/* 객체는 여러가지 속성의 데이터를 가진다. */
 const pale = {
     name: ["이창민", "이재백", "한우석"],
     age: 23,
     male: true,
-    female: false
+    female: false,
+    hey: function(ㅋㅋ) {
+        console.log("알빠노" + ㅋㅋ)
+    }
 };
-console.log(pale);
-console.log(pale.name[0]);
-console.log(pale.male);
-pale.name = "이재백";//const는 pale이 가리킬 주소이지 요소가 아님. 변경 가능
-console.log(pale.name[1]);//재
-pale.location = "discord";
-console.log(pale); //location 추가됨.
+
+pale.hey("ㅋㅋ");
+pale.hey("ㅎㅎ");
+
+function hey(name) {
+    console.log(name + "멍청이");
+}
+
+hey("이창민");
+hey("이재백");
+hey("한우석");
