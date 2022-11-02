57d31d11e541eb35c7c05211caab6c87b102690f
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-11-02 19:17:04 +0900

    Conditionals Part

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-calculator = {
-    add: function(a, b) {
-        return a + b;
-    },
-    sub: function(a, b) {
-        return a - b;
-    },
-    mul: function(a, b) {
-        return a * b;
-    },
-    div: function(a, b) {
-        return a / b;
-    }
-};
-
-let a = parseInt(prompt("Please input first integer."));
-let b = parseInt(prompt("Please input second integer."));
-
-const add = calculator.add(a, b);
-const sub = calculator.sub(a, b);
-const mul = calculator.mul(a, b);
-const div = calculator.div(a, b);
-
-console.log(add, sub);
-console.log(calculator.add(add, sub));
+const age = parseInt(prompt("How old are you?"));
+//우측이 먼저 실행되고 그 값이 저장된 주소를 age에 할당하는 것이라 되는 것
+//이건 c++도 마찬가지임. 즉 둘 다 const 변수의 값 할당이 런타임 시간에 일어나는 것 같다.
+//알아보니 실제로 컴파일러는 const 변수가 compile time constants인지 run time constants인지
+//컴파일 단계에서 구분하는 것 같다.
+if (isNaN(age) || age < 0) {
+    console.log("Please write a real positive number");
+}
+else if (age < 18) {
+    console.log("Go to your mommy");
+}
+else if (age >= 18 && age <= 50) {
+    console.log("You can drink");
+}
+else {
+    console.log("I love you");
+}
+//===는 자동 형변환을 용납하지 않는다.
+//parseInt는 숫자 말고 다른게 들어오면 NaN를 리턴한다.
+//기타는 다 C++과 동일함. || && 등등
