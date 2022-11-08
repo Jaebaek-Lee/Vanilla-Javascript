8ce927e3de91d039d324d0867c6b83cd874673f5
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-11-08 21:38:47 +0900

    Login_1

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-//div.header:nth-child(2) h1
-//header라는 클래스 네임이 있는 두 번째 div의 첫 번째 h1태그
-const head = document.querySelector("div.header:nth-child(1) h1");
-//이 head는 진짜 document.head랑 다르다. 그냥 내가 지은 이름일 뿐...
-console.dir(head);
-console.log(head.style.color);
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-//head.className = clickedClass; -> 모든 classname을 이걸로 교체해버림.
-        //만약 html에서 이미 클래스가 정의되어 있다면 그걸 없애버림. 문제 됨.
-
-function click() {
-    // const clickedClass = "active"; //수정 편의 + 오류 발생 낮춤
-    // if (head.classList.contains(clickedClass)) {
-    //     head.classList.remove(clickedClass);
-    // }
-    // else head.classList.add(clickedClass);
-    head.classList.toggle("active");
+function onLoginBtnClick() {
+    console.log("hello", loginInput.value);
+    //입력이 없어도 hello를 출력하는 문제 발생.
 }
 
-head.addEventListener("click", click);
+loginButton.addEventListener("click", onLoginBtnClick);
